7d5910214764017109d280551aee6426d2f61989
hrodriguez <hrodriguez.a@tomza.com>
2020-05-27 16:45:31 -0600

    Agregar archivos de proyecto.

---
+++ cfdi/models/CFDi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cfdi.models
+{
+    public class CFDi
+    {
+        public int idSucursal { get; set; }
+        public string sucursal { get; set; }
+        public string rfcSucursal { get; set; }
+        public string nombreReceptor { get; set; }
+        public 
+    }
+}
