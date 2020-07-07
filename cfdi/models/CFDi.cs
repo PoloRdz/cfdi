e4fb36671d0adb95879509575d6300bfb9b975da
Polo Rodriguez <prhiroal@gmail.com>
2020-07-07 16:45:48 -0600

    a

--- cfdi/models/CFDi.cs
+++ cfdi/models/CFDi.cs
@@ -7,25 +7,25 @@ namespace cfdi.Models
 {
     public class CFDi
     {
-        public Emisor emisor { get; set; }
-        public Receptor receptor { get; set; }
-        public CFDiRelacionado[] relaciones { get; set; }
-        public string usoCFDi { get; set; }
-        public int idFolio { get; set; }
-        public int idMov { get; set; }
-        public string tipoCompra { get; set; }
-        public string tipoVenta { get; set; }
-        public string estadoFolio { get; set; }
-        public string serie { get; set; }
-        public int folio { get; set; }
-        public DateTime fecha { get; set; }
-        public List<Concepto> conceptos { get; set; }
-        public Pagos pagos { get; set; }
+        public Emisor emisor { get; set; }//si
+        public Receptor receptor { get; set; }//si
+        public CFDiRelacionado[] relaciones { get; set; }//no
+        public string usoCFDi { get; set; }//si
+        public int idFolio { get; set; }//si
+        public int idMov { get; set; }//si
+        public string tipoCompra { get; set; }//si
+        public string tipoVenta { get; set; }//si
+        public string estadoFolio { get; set; }//si
+        public string serie { get; set; }//si
+        public int folio { get; set; }//si
+        public DateTime fecha { get; set; }//si
+        public List<Concepto> conceptos { get; set; }//no
+        public Pagos pagos { get; set; }//no
         public double subtotal { get; set; }
         public double totalImp { get; set; }
         public double total { get; set; }
-        public double saldo { get; set; }
-        public string importeLetra { get; set; }
+        public double saldo { get; set; }//no
+        public string importeLetra { get; set; }//no
         public string mPago { get; set; }
         public string formaPago { get; set; }
         public string moneda { get; set; }
@@ -37,6 +37,6 @@ namespace cfdi.Models
         public string selloEmisor { get; set; }
         public string folioFiscal { get; set; }
         public DateTime fechaCert { get; set; }
-        public string xml { get; set; }
+        public string xml { get; set; }//no
     }
 }
